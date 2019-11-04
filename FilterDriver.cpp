7dded3762859066e35541ab659454743ad0b0566
Dannie-FAMU <57149288+Dannie-FAMU@users.noreply.github.com>
2019-11-03 23:14:02 -0500

    Update FilterDriver.cpp

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "BaseballStatistic.h"
+#include "Date.h"
+#include "Filter.h"
+
+using namespace std;
+
+int main()
+{
+    ifstream baseIn("BStats.csv");
+    vector<string> List;
+    string holder;
+    while (baseIn.eof() == 0)
+    {
+        getline(baseIn, holder);
+        List.push_back(holder);
+    }
+    
+    
+    
+    return 0;
+}
 
