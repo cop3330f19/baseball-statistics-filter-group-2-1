be63c7cf7f6e3d685e981df5e7e09b292d8c8453
rhiana1long <57336735+rhiana1long@users.noreply.github.com>
2019-11-04 23:06:09 -0500

    Update Filter.h

--- Filter.h
+++ Filter.h
@@ -1,1 +1,24 @@
 
+#ifndef FILTER_H
+#define FILTER_H
+
+
+#include "BaseballStatistic.h"
+#include <string>
+#include <vector>
+
+
+
+class Filter
+{
+   public:
+   
+   static void swap(vector<BaseballStatistic>&,int,int);
+   
+   static void namesort(vector<BaseballStatistic>&);
+   
+   static int search(vctor<BaseballStatistic>&,std::string, std::string, std::string);
+   
+         
+};
+#endif
