ea689d7dcd44a2fd016f820fef14f6f500d4fbe7
Dannie-FAMU <57149288+Dannie-FAMU@users.noreply.github.com>
2019-11-07 13:51:18 -0500

    Add files via upload

--- FilterDriver.cpp
+++ FilterDriver.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 
 #include "BaseballStatistic.h"
 #include "Date.h"
@@ -16,50 +17,111 @@ void loadFile(vector<BaseballStatistic>&);
 void printHeading();
 
 
-
 int main()
 {
     
-    vector<players> baseList;
-   // created file stream to open data from baseball stats 
+    vector<BaseballStatistic> baseList;
+    
     ifstream inFile;
     inFile.open("BStats.csv");
     
+    string team_name, fname, lname, pos;
+    int jersey_num, b_year, b_month, b_day, atbat, t_HR, t_RBI, t_SB;
+    char batArm, throwArm;
+    double batAvg, t_OPS, t_ERA;
     
-   
-   // reading in the line 
-    while (inFile) {
-        string player_line;
-        inFile >> player_line;
-        istringstream ss(player_line);
+    string holder;
+    int c = 0;
+    while (inFile.eof() == 0) 
     {
-     //adding from the line into temporary values  
-        while(ss)
-       {
-        getline(ss, team_name, ',');
-        getline(ss, jersey_num, ',');
-        getline(ss, fname, ',');
-        getline(ss, lname, ',');
-        getline(ss, b_year, ',');
-        getline(ss, b_month, ',');
-        getline(ss, b_day, ',');
-        getline(ss, batArm, ',');
-        getline(ss, throwArm, ',');
-        getline(ss, Atbats, ',');
-        getline(ss, batAvg, ',');
-        getline(ss, t_HR, ',');
-        getline(ss, t_RBI, ',');
-        getline(ss, t_SB, ',');
-        getline(ss, t_OPS, ',');
-        getline(ss, t_ERA, ',');
-        getline(ss, pos, ',');
-  // pushing players into vector created
-        players.push_back(temp);
-       }
+        getline(inFile, team_name, ',');
+        
+        //Input jersey number into holder string
+        getline(inFile, holder, ',');
+        //Stream holder value to string stream
+        stringstream convertj(holder); 
+        //puts string converted to int to jersey_num
+        convertj >> jersey_num;
+        
+        
+        getline(inFile, fname, ',');
+        getline(inFile, lname, ',');
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertBY(holder);
+        convertBY >> b_year;
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertBM(holder);
+        convertBM >> b_month;
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertBD(holder);
+        convertBD >> b_day;
+        
+        char held;
+        inFile.get(batArm);
+        inFile.get(held); //For the ',' after
+        
+        inFile.get(throwArm);
+        inFile.get(held); //For the ',' after
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertAB(holder);
+        convertAB >> atbat;
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertBA(holder);
+        double avgbats;
+        convertBA >> avgbats;
+        
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertHR(holder);
+        convertHR >> t_HR;
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertRBI(holder);
+        convertRBI >> t_RBI;
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertSB(holder);
+        convertSB >> t_SB;
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertOPS(holder);
+        double opsin;
+        convertOPS >> opsin;
+        
+        
+        getline(inFile, holder, ',');
+        stringstream convertERA(holder);
+        convertERA >> t_ERA;
+        
+        
+        getline(inFile, pos, ',');
+        
+        BaseballStatistic first (fname, lname, pos, jersey_num, team_name,
+                               atbat, t_HR, t_RBI, batArm, throwArm,
+                               t_SB, avgbats, b_month, b_day, b_year,
+                               opsin, t_ERA);
+        
+        //Left Here!!!
+        /*(baseList+c) = first;
+        baseList[c].setFirstName(first.getFirstName);*/
+        
+        baseList.push_back(first);
     }
     
-   
     
     return 0;
 }
-
